23b797b03b81dff318eee1736cdb4027fd62ec6d
michal <michal.blaszczyk1997@gmail.com>
2019-12-15 18:35:47 +0100

    Add database.sql script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,28 @@
+
+insert into drinks(id_drink, name, price) values(1, 'Water', 4);
+insert into drinks(id_drink, name, price) values(2, 'Wine', 20);
+insert into drinks(id_drink, name, price) values(3, 'Juice', 10);
+insert into drinks(id_drink, name, price) values(4, 'Beer', 15);
+
+insert into desserts(id_desserts, name, price) values(1, 'Cake', 16);
+insert into desserts(id_desserts, name, price) values(2, 'Tiramisu', 23.0);
+insert into desserts(id_desserts, name, price) values(3, 'Ice-cream', 13.0);
+insert into desserts(id_desserts, name, price) values(4, 'Kremowka', 21.0);
+insert into desserts (id_desserts, NAME, PRICE) values(5, 'Sernik', 14.0); 
+insert into desserts (id_desserts, NAME, PRICE) values(6, 'Mexican Cake', 16.0);  
+
+
+insert into maincourses(id_maincourses, name, price) values(1, 'Schabowy', 31.0);
+insert into maincourses(id_maincourses, name, price) values(2, 'Pizza', 45.0);
+insert into maincourses(id_maincourses, name, price) values(3, 'Pierogi', 25.0); 
+insert into maincourses(id_maincourses, name, price) values(4, 'Buritto', 35.0); 
+insert into maincourses(id_maincourses, name, price) values(5, 'Pasta', 25.0); 
+insert into maincourses(id_maincourses, name, price) values(6, 'Taco', 28.0);
+
+
+insert into lunches (ID,NAME,CUISINE,MAINCOURSE_ID,DESSERT_ID) values(2,'Polish ',0,2,2); 
+insert into lunches (ID,NAME,CUISINE,MAINCOURSE_ID,DESSERT_ID) values(1,'Special Numero Uno',2,1,1); 
+insert into lunches (ID,NAME,CUISINE,MAINCOURSE_ID,DESSERT_ID) values(3,'Mexican Specials',1,3,3); 
+insert into lunches (ID,NAME,CUISINE,MAINCOURSE_ID,DESSERT_ID) values(4,'Italian Food',2,4,4);
+insert into lunches (ID,NAME,CUISINE,MAINCOURSE_ID,DESSERT_ID) values(5,'Basic Polish',0,5,5);  
+insert into lunches (ID,NAME,CUISINE,MAINCOURSE_ID,DESSERT_ID) values(6,'Basic Mexican',1,6,6); 
